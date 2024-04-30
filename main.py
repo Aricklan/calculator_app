f8a56521b6cb9bc85d84d95426661cee3685fdd1
Aricklan <ands2233t@gmail.com>
2024-04-30 13:26:26 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+class Calculator: 
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Calculator")
+
+        self.window.geometry("375x667")
+        self.window.resizable(0, 0)
+        self.window.configure(bg="black")
+
+    def run(self):
+        self.window.mainloop()
+
+
+if __name__ == "__main__":
+    calc = Calculator()
+    calc.run()
